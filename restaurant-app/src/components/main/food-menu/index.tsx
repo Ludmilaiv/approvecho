47095e4b2cf6cb8fe8c11aef7153511c7df029e7
Ludmila <Ludiky30@gmail.com>
2022-03-21 04:40:42 +0700

    food-menu section

---
+++ restaurant-app/src/components/main/food-menu/index.tsx
@@ -0,0 +1,67 @@
+import classNames from 'classnames';
+import React, {useState} from 'react';
+import './style.sass';
+
+const categories = [
+  {id: 1, title: 'Салаты'},
+  {id: 2, title: 'Винная карта'},
+  {id: 3, title: 'Закуски'},
+  {id: 5, title: 'Супы'},
+  {id: 6, title: 'Десерты'},
+  {id: 7, title: 'Напитки'},
+];
+
+const menu = [
+  {id: 1, category: 1, title: 'Цезарь', img: 'cezar.jpg', desc: '(Филе кур, бекон, черри, яйцо перепелиное, руккола, Mozzarella, cоус Итальянский)', massa: 324, unit: 'гр',  price: 100},
+  {id: 2, category: 1, title: 'Антонио', img: 'antonio.jpg', desc: '(Филе куриное, черри, бруснично-грушевый соус, сладкий перец, Mozzarella)', massa: 324, unit: 'гр', price: 100},
+  {id: 3, category: 1, title: 'Греческий', img: 'grecheskiy.jpg', desc: '(Филе куриное, черри, бруснично-грушевый соус, сладкий перец, Mozzarella)', massa: 324, unit: 'гр', price: 100},
+  {id: 4, category: 1, title: 'Цезарь', img: 'cezar.jpg', desc: '(Филе кур, бекон, черри, яйцо перепелиное,руккола, Mozzarella, cоус Итальянский)', massa: 324, unit: 'гр', price: 100},
+  {id: 5, category: 1, title: 'Антонио', img: 'antonio.jpg', desc: '(Филе куриное, черри, бруснично-грушевый соус, сладкий перец, Mozzarella)', massa: 324, unit: 'гр', price: 100},
+  {id: 6, category: 1, title: 'Греческий', img: 'grecheskiy.jpg', desc: '(Филе куриное, черри, бруснично-грушевый соус, сладкий перец, Mozzarella)', massa: 324, unit: 'гр', price: 100},
+];
+
+export function FoodMenu() {
+  const [activeCategory, setActiveCategory] = useState(categories[0]);
+
+  return (
+    <section className='food-menu container'>
+      <div className='food-menu__categories'>
+        <h2 className='food-menu__title'>Меню</h2>
+        <ul className='food-menu__categories-list'>
+
+          {categories.map(category =>
+            <li key={category.id} className={classNames('food-menu__categories-item', activeCategory.id === category.id && 'food-menu__categories-item_active')} onClick={() => setActiveCategory(category)}>
+              {category.title}
+            </li>
+          )}
+
+        </ul>
+      </div>
+      <div className='food-menu__category-wrp'>
+        <h3 className='food-menu__category-title'>Салаты</h3>
+        <ul className='food-menu__list'>
+
+          {menu.map(item => 
+            item.category === activeCategory.id && <li key={item.id} className='food-menu__item'>
+              <div>
+                <div className='food-menu__item-img-box'>
+                  <img className='food-menu__item-img' src={`menu_img/${item.img}`} alt="photo" />
+                </div>
+                <h4 className='food-menu__item-title'>{item.title}</h4>
+                <p className='food-menu__item-desc'>
+                  {item.desc}
+                </p>
+              </div>
+              <div className='food-menu__item-footer'>
+                <span className='food-menu__item-massa'>324гр</span>
+                <span className='food-menu__item-price'>100 ₽</span>
+              </div>
+            </li>
+          )}
+
+        </ul>
+      </div>
+      
+    </section>
+  );
+}
--- restaurant-app/src/components/main/index.tsx
+++ restaurant-app/src/components/main/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Booking } from './booking';
 import { Contacts } from './contacts';
+import { FoodMenu } from './food-menu';
 
 export function Main() {
   return (
     <div>
       <Booking />
+      <FoodMenu />
       <Contacts />
       <Booking classMod="light"/>
     </div>
